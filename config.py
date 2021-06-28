e55651867716e8811a229454d3381b3cd74d195e
Shishir Pandey <shishir.py@gmail.com>
2021-06-27 23:26:53 -0400

    fixed None strip() issue

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ class Config(object):
     }
     
     # Google One tap
-    GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None).strip()
-    GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None).strip()
+    GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "").strip()
+    GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "").strip()
     GOOGLE_DISCOVERY_URL = (
                 "https://accounts.google.com/.well-known/openid-configuration"
             )
